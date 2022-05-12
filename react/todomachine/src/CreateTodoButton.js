b341d812c67fd03937d0a29ad3a647635db01233
Diego Naranjo <diegoanr40@hotmail.com>
2022-05-12 14:12:13 -0500

    agregando interactividad al proyecto

--- react/todomachine/src/CreateTodoButton.js
+++ react/todomachine/src/CreateTodoButton.js
@@ -1,9 +1,20 @@
 import React from 'react';
 import './CreateTodoButton.css';
 
+
 function CreateTodoButton(props) {
+
+  const onClickButton = (msg) => {
+    alert(msg)
+  }
+
   return (
-    <button className="CreateTodoButton">+</button>
+    <button 
+      className="CreateTodoButton"
+      onClick={() => onClickButton ('Aqui hay un mensaje')}
+    >
+      +
+    </button>
   );
 }
 
